657c67546f31a877cb323379af1fc04c0ae20b55
Katsuo <madarauchiha76189@gmail.com>
2026-07-21 18:57:39 -0300

    Update mods.js

--- mods.js
+++ mods.js
@@ -3,8 +3,8 @@ const mods = [
 {
     nome: "Follower Light And Shade",
     imagem: "imagens/mod1.jpg",
-    descricao: "Adicione uma nova seguidora chamada Eris.",
-    pagina: "mods/Follower%20Light%20And%20Shade.html",
+    descricao: "Adicione uma nova seguidora chamada Eris para acompanhá-lo em Skyrim.",
+    pagina: "Follower%20Light%20And%20Shade.html",
     categoria: "Companions"
 }
 
