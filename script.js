bc205e7125b38a02626acd4efd024803a34401b3
Katsuo <madarauchiha76189@gmail.com>
2026-07-21 17:23:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 document.querySelector("button").addEventListener("click", function() {
     alert("Em breve o download estará disponível!");
 });
+const pesquisa = document.getElementById("pesquisa");
+
+if (pesquisa) {
+    pesquisa.addEventListener("keyup", function () {
+
+        const texto = pesquisa.value.toLowerCase();
+        const mods = document.querySelectorAll(".card-mod");
+
+        mods.forEach(function (mod) {
+
+            const nome = mod.querySelector("h3").textContent.toLowerCase();
+
+            if (nome.includes(texto)) {
+                mod.style.display = "";
+            } else {
+                mod.style.display = "none";
+            }
+
+        });
+
+    });
+}
