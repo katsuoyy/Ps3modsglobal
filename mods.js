ec914d702402e3a167069b62df9a4e158741b717
Katsuo <madarauchiha76189@gmail.com>
2026-07-21 18:48:19 -0300

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,11 @@
+const mods = [
+
+{
+    nome: "Follower Light And Shade",
+    imagem: "imagens/mod1.jpg",
+    descricao: "Adicione uma nova seguidora chamada Eris.",
+    pagina: "mods/Follower%20Light%20And%20Shade.html",
+    categoria: "Companions"
+}
+
+];
